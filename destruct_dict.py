5038c0def0e0c1f3c30bdf9c6024e3c06589bb47
wangfusheng <ostrich_king@163.com>
2018-10-19 00:20:39 +0800

    finish recursive destuct dict.

--- destruct_dict.py
+++ destruct_dict.py
@@ -1,3 +1,15 @@
+def check_key(key: str) -> bool:
+    return len(key.split(".")) == 1
+
+
+def check_dicts(values):
+
+    for v in values:
+        for k in v:
+            if check_key(k):
+                return False
+    return True
+
 
 def descruct_dict(source: dict) -> dict:
     target = dict()
@@ -6,21 +18,34 @@ def descruct_dict(source: dict) -> dict:
         k_list = k.split(".", 1)
         if len(k_list) > 1:
             if not target.get(k_list[0]):
-                target[k_list[0]] = descruct_dict({k_list[1]: v})
+                target[k_list[0]] = {k_list[1]: v}
             else:
-                target[k_list[0]].update(**descruct_dict({k_list[1]: v})
+                target[k_list[0]].update({k_list[1]: v})
         else:
             target[k_list[0]] = v
-    # for k, v in target
+    """
+    values = filter(lambda v: isinstance(v, dict), target.values())
+
+    _finish = check_dicts(values)
+    if not _finish:
+        print("-" * 100)
+        for k, v in target.items():
+            if isinstance(v, dict):
+                target[k] = descruct_dict(v)
 
     return target
-
+    """
+    for k, v in target.items():
+        if isinstance(v, dict) and not check_dicts(v):
+            target[k] = descruct_dict(v)
+    return target
 
 if __name__ == "__main__":
     source = {
         "a.b": 1,
         "c.a": 2,
-        "d.a.e": 4,
+        "c.t.b": 6, 
+        "g.d.a.e.f": 4,
         "c.b": 5
     }
 
